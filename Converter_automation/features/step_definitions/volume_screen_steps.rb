3418aad207c4285b089da57e849e22028f838f26
OLI2018 <37943857+OLI2018@users.noreply.github.com>
2018-05-03 16:34:17 -0700

    Second Swipe added

--- Converter_automation/features/step_definitions/volume_screen_steps.rb
+++ Converter_automation/features/step_definitions/volume_screen_steps.rb
@@ -1,5 +1,5 @@
 When(/^I swipe in the menu$/) do
-  action = Appium::TouchAction.new.swipe(start_x: 0.01 , start_y: 0.5 , end_x: 0.9 , end_y: 0.5 , duration: 500)
+  action = Appium::TouchAction.new.swipe(start_x: 0.01, start_y: 0.5, end_x: 0.9, end_y: 0.5, duration: 800)
   action.perform
 end
 
@@ -8,3 +8,15 @@ Then(/^I see app menu$/) do
   find_element(id: "design_navigation_view").find_element(xpath: "//android.widget.CheckedTextView[@text='Cooking']")
   find_element(id: "design_navigation_view").find_element(xpath: "//android.widget.CheckedTextView[@text='Currency']")
 end
+
+
+And(/^I select "([^"]*)" from menu$/) do |value|
+  action = Appium::TouchAction.new.swipe(start_x: 0.5, start_y: 0.85, end_x: 0.5, end_y: 0.5 , duration: 200)
+  action.perform
+  find_element(id: "design_navigation_view").find_element(xpath: "//android.widget.CheckedTextView[@text='#{value}']").click
+end
+
+Then(/^I land on "([^"]*)" screen$/) do |value|
+  find_element(id: "toolbar").find_element(xpath: "//android.widget.TextView[@text='#{value}']")
+end
+
